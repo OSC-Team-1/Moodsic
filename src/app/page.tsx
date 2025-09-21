fb716e63a4dca4b49c0f1208b643fd599d544d2e
K1mmyn <125098192+K1mmyn@users.noreply.github.com>
2025-09-21 13:14:56 +1200

    prompt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,19 @@ export default function Home() {
   const ai = new GoogleGenAI({ apiKey: API_KEY });
 
   const handleOnResult = async (voiceText: string) => {
+    const base64VideoFile = fs.readFileSync("path/to/small-sample.mp4", {
+      encoding: "base64",
+    });
+
+    const contents = [
+      {
+        inlineData: {
+          mimeType: "video/mp4",
+          data: base64VideoFile,
+        },
+      },
+      { text: "Please summarize the video in 3 sentences." },
+    ];
     const prompt = `
         Input voice: "${voiceText}"
         `;
@@ -22,14 +35,20 @@ export default function Home() {
         thinkingConfig: {
           thinkingBudget: -1,
         },
-        systemInstruction: `You are working as a sentiment analysis assistant. Based on the user's voice-to-text and video data,
-        determine the user mood. The moods you are to choose from are "joyful", "calm",
-        "excited", "sad", "angry", "fearful", "disgusted", "surprised", "curious", "conflicted" respond with only the mood.
-        If they are in between two moods, decide concretely which one they are in, you are only allowed to pick from the list
+        systemInstruction: `You are working as a sentiment analysis assistant who can recommend music. Based on the 
+        user's voice-to-text and video data, determine the user mood and a song you would recommend based on their 
+        favourite genre and artist. The moods you are to choose from are "joyful", "calm", "excited", "sad", "angry", 
+        "fearful", "disgusted", "surprised", "curious", "conflicted" respond with only the mood. If they are in between 
+        two moods, decide concretely which one they are in, you are only allowed to pick from the list
+
+        The user likes to listen to artist such as "//! PLACE HOLDER"
+        the user likes to listen to genres such as "//! PLACE HOLDER"
+
         `,
       },
     });
     setMood(response.text);
+    const API_URL = `https://api.spotify.com/v1/search?q=never+gonna+give+you+up+artist%3ARick+Astley&type=track&market=NZ&limit=5&offset=0`;
   };
 
   const initSpeechRecognition = () => {
